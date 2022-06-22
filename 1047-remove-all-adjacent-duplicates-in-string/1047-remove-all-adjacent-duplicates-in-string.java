c9780833f67945f457b760b26f2058cc832ccb94
Ayush Sharma <79182175+Ayushaff@users.noreply.github.com>
2022-06-22 10:59:25 +0530

    Time: 1121 ms (5.01%), Space: 61.7 MB (18.64%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -1,6 +1,6 @@
 class Solution {
     public String removeDuplicates(String s) {
-        StringBuilder sb = new StringBuilder();
+       StringBuilder sb = new StringBuilder();
         for (char c : s.toCharArray()) {
             if (sb.length() > 0 && sb.charAt(sb.length() - 1) == c) {
                 System.out.println("if");
