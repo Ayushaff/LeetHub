a09ff699e8899ec7489486cccaf5ef8b4c9f8417
Ayush Sharma <79182175+Ayushaff@users.noreply.github.com>
2022-06-25 08:40:53 +0530

    Time: 1 ms (99.82%), Space: 50.4 MB (82.37%) - LeetHub

--- 344-reverse-string/344-reverse-string.java
+++ 344-reverse-string/344-reverse-string.java
@@ -1,17 +1,29 @@
 class Solution {
+    //3
+    public void reverse(char []arr,int s,int e){
+        if (s >=e)
+    return;
+        char temp=arr[s];
+        arr[s]=arr[e];
+        arr[e]=temp;
+        reverse(arr,s+1,e-1);
+    }
     public void reverseString(char[] s) {
+        //1
        /* List<Character> vector = new ArrayList<>();
         for(int i=0;i<s.length;i++)vector.add(s[i]);
             Collections.swap(vector,0,s.length);
     }*/
-        int ap=0;
+       /* int ap=0;
         int bp=s.length-1;
         while(ap<bp){
             char temp=s[ap];
             s[ap]=s[bp];
             s[bp]=temp;
             ap++;
-            bp--;
-        }
+            bp--;*/
+        
+        Solution ss=new Solution();
+        ss.reverse(s,0,s.length-1);
     }
 }
