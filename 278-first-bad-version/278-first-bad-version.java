fa53a3e256f11e3b59db9ad678264b68bb589fec
Ayush Sharma <79182175+Ayushaff@users.noreply.github.com>
2022-06-28 11:19:19 +0530

    Time: 18 ms (57.20%), Space: 41.4 MB (15.66%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.java
+++ 278-first-bad-version/278-first-bad-version.java
@@ -5,7 +5,26 @@ public class Solution extends VersionControl
 {
     public int firstBadVersion(int n)   
     {
-        int lo=1,hi =n,fbv=-1;
+     long lo=1;
+     long hi=n;
+     long mid;
+     long result =n;
+     while(lo<=hi)
+        {  
+        mid=(lo+hi)/2;
+        if(isBadVersion((int)mid)==true) 
+        {
+            result=mid;
+            hi=mid-1;
+        }
+        else lo=mid+1;
+        
+        }
+    return (int)result;
+     }
+}
+       
+       /*int lo=1,hi =n,fbv=-1;
         int mid=0;
         while(lo<=hi)
         {
@@ -22,4 +41,4 @@ public class Solution extends VersionControl
         }
         return fbv;
     }
-}
+}*/
