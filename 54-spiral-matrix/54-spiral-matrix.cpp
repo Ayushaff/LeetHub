f43c46831e7fce11eab1d29e6aaa607cae6ef05f
Ayush Sharma <79182175+Ayushaff@users.noreply.github.com>
2022-07-01 21:19:09 +0530

    Time: 3 ms (43.05%), Space: 6.9 MB (71.71%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.cpp
+++ 54-spiral-matrix/54-spiral-matrix.cpp
@@ -1,6 +1,64 @@
 class Solution {
 public:
-    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+    vector<int> spiralOrder(vector<vector<int>> &matrix)
+    {
+        // 2
+        vector<int> ans;
+        int row = matrix.size();
+        int col = matrix[0].size();
+
+        int count = 0;
+        int totl = row * col;
+
+        // index initialize
+        int startingRow = 0;
+        int startingCol = 0;
+        int endingRow = row - 1;
+        int endingCol = col - 1;
+
+        while (count < totl)
+        {
+            // print startingRow
+            // startingcol se ending col tak loop
+
+            for (int index = startingCol; count < totl && index <= endingCol; index++)
+            {
+                ans.push_back(matrix[startingRow][index]);
+                count++;
+            }
+            startingRow++;
+
+            // print endingCol
+            // startingRow se ending row tak loop
+            for (int index = startingRow; count < totl && index <= endingRow; index++)
+            {
+                ans.push_back(matrix[index][endingCol]);
+                count++;
+            }
+            endingCol--;
+
+            // print endingRow
+            // endingCol se startingcol tak loop
+            for (int index = endingCol; count < totl && index >= startingCol; index--)
+            {
+                ans.push_back(matrix[endingRow][index]);
+                count++;
+            }
+            endingRow--;
+
+            // print staringCol
+            // endingRow se startingRow tak loop
+            for (int index = endingRow; count < totl && index >= startingRow; index--)
+            {
+                ans.push_back(matrix[index][startingCol]);
+                count++;
+            }
+            startingCol++;
+        }
+        return ans;
+    }
+        
+        /* 1
         vector<int>ans;
         int N=matrix.size();
         int M=matrix[0].size();
@@ -45,6 +103,6 @@ public:
             }
             dir=(dir+1)%4;
         }
-        return ans;
-    }
+        return ans;*/
+        
 };
