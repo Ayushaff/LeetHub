d34251f153114acaacea91b5a14b17284d648ea6
Ayush Sharma <79182175+Ayushaff@users.noreply.github.com>
2022-07-06 16:31:46 +0530

    Time: 20 ms (6.16%), Space: 40 MB (82.34%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int fib(int n) {
+        
+        //naive approach
+        // 1
+        //1-Liner
+         return n<=1 ? n :  fib(n-1) + fib(n-2);
+        
+        
+        
+        //3
+         /*if(n==0)return 0;
+        else if(n==1)return 1;
+        else return f(n-1)+f(n-2);*/
+    }
+}
