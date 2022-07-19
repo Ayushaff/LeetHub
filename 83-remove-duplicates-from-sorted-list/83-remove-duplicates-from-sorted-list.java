d8de164401eade54beda955139b86052548a0717
Ayush Sharma <79182175+Ayushaff@users.noreply.github.com>
2022-07-19 11:08:33 +0530

    Time: 1 ms (77.89%), Space: 44.9 MB (5.60%) - LeetHub

--- 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.java
+++ 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.java
@@ -8,35 +8,19 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+
 class Solution {
-     public ListNode deleteDuplicates(ListNode head) {
-         ListNode fakehead=head;
-         
-         while(fakehead!= null && fakehead.next!= null ){
-             if(fakehead.val==fakehead.next.val){
-                 fakehead.next=fakehead.next.next;
-             }
-             else{
-             fakehead=fakehead.next;    
-             }
-         }
-         return head;
-     }
-}
-    
-/*   
+    public ListNode deleteDuplicates(ListNode head) {
         if(head==null || head.next==null) return head;
         ListNode curr=head;
         
         while(curr!=null){
             if(curr.next != null && (curr.val == curr.next.val)){
+                
                 curr.next=curr.next.next;
                 
-                /*
-//                ListNode next_next=curr.next.next;
-  //              ListNode nodeTodlt=curr.next;
-                //delete that node delete(nodeTodlt);
-    //            curr.next=next_next;
+                
             }
             else{
                 curr=curr.next;
@@ -45,4 +29,4 @@ class Solution {
         return head;
     }
 }
-*/
+
