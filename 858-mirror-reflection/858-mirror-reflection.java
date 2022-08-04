ddf4c819b18baa62ed762fe013606cd96e57a15e
Ayush Sharma <79182175+Ayushaff@users.noreply.github.com>
2022-08-04 22:09:06 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (34.72%) - LeetHub

--- 858-mirror-reflection/858-mirror-reflection.java
+++ 858-mirror-reflection/858-mirror-reflection.java
@@ -1,5 +1,6 @@
 class Solution {
     public int mirrorReflection(int p, int q) {
+       // https://leetcode.com/problems/mirror-reflection/discuss/146336/Java-solution-with-an-easy-to-understand-explanation
         while(p%2==0 && q%2==0){
             p/=2;
             q/=2;
