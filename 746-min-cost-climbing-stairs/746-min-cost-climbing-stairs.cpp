835cfc3f493464ad0dd6db1be4e8c0a77aab27ab
Ayush Sharma <79182175+Ayushaff@users.noreply.github.com>
2022-08-18 21:32:24 +0530

    Time: 103 ms (5.13%), Space: 240.2 MB (5.07%) - LeetHub

---
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+
+    int solve(vector<int> &cost ,int n){
+            if(n==0) return cost[0];
+            if(n==1) return cost[1];
+            
+            int ans=cost[n] +min(solve(cost,n-1),solve(cost,n-2));
+            return ans;
+        }
+    int solve2(vector<int>cost ,int n,vector<int>& dp){
+        //basecase step1
+            if(n==0) return cost[0];
+            if(n==1) return cost[1];
+            //step3
+        if(dp[n] != -1) return dp[n];
+        //memoziation step2
+            dp[n]=cost[n] +min(solve2(cost,n-1,dp),solve2(cost,n-2,dp));
+            return dp[n];
+        }
+        int minCostClimbingStairs(vector<int>& cost) {
+            int n=cost.size();
+            vector<int> dp(n+1,-1);
+            int ans=min(solve2(cost,n-1,dp),solve2(cost,n-2,dp));
+            return ans;
+}
+};
