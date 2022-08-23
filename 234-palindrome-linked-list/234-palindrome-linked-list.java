81ee50f49f71bb979496efffdf64e767e84a25f6
Ayush Sharma <79182175+Ayushaff@users.noreply.github.com>
2022-08-23 10:40:00 +0530

    Time: 4 ms (97.84%), Space: 53 MB (98.54%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.java
+++ 234-palindrome-linked-list/234-palindrome-linked-list.java
@@ -1,30 +1,76 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+/*class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}*/
+
 class Solution {
-    public boolean isPalindrome(ListNode head) {
-        
-     
-    ListNode ptr=head;
-        Stack<Integer>stk=new Stack<>();
-        while(ptr!=null){
-            stk.push(ptr.val);
-            ptr=ptr.next;
-            
+    public boolean isPalindrome(ListNode head)
+    {
+        // find middle
+        // reverse the right half
+        // comapare right half with the left half
+        if (head == null || head.next == null)
+            return true;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        // find middle of LL
+        while (fast.next != null && fast.next.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        ptr=head;
-        
-        while(ptr!=null){
-            if(ptr.val!=stk.pop())return false;
-            ptr=ptr.next;    
+
+        // reverse the right half
+        slow.next = reverseLL(slow.next); // slow.next got the reverse right half
+
+        // move slow to right half
+        slow = slow.next;
+
+        while (slow != null) 
+        {
+            if (head.val == slow.val) 
+            {
+                head = head.next;
+                slow = slow.next;
+            } else
+                return false;
         }
         return true;
+    }     
+
+  public ListNode reverseLL(ListNode head) 
+  {     
+
+        if (head == null || head.next == null)
+            return head;
+
+        ListNode curr = head;
+        ListNode prev = null;
+
+        while (curr != null) 
+        {
+
+            ListNode tmp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = tmp;
+        }
+        // if(curr==null) System.out.println("asd");
+        return prev; // is at last node became as a new head
     }
+       
 }
