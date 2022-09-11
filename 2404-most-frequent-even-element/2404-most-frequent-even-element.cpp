8b90021a6c25041fe859e9d4cd2826fb6c9ef18a
Ayush Sharma <79182175+Ayushaff@users.noreply.github.com>
2022-09-11 13:06:55 +0530

    Time: 163 ms (25.00%), Space: 38.7 MB (25.00%) - LeetHub

---
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+        //https://www.youtube.com/watch?v=fyuHcXG9iQI
+      
+    int mostFrequentEven(vector<int>& nums) {
+            map<int,int>counts;
+            
+            for(int i:nums){
+                if(i%2==0)
+                   counts[i]++;    
+            }
+            
+            int ans=-1 ,fre=INT_MIN; 
+            
+            for(auto i: counts){
+                //cout<<i.first <<" "<<i.second<<endl;
+                    if(i.second>fre){
+                            ans=i.first, fre=i.second;
+                    }
+            }
+            return ans;
+    }
+};
+ /*     int ans;
+            unordered_map<int,int>counts;
+            priority_queue<pair<int,int>, vector<pair<int,int>>,greater<pair<int,int>>>min_heap;
+            
+            for(auto i:nums){
+                if(nums[i]%2==0)
+                   counts[i]++;    
+            }
+            for(auto &i: counts){
+                    
+                min_heap.push({i.first,i.second});
+                    int j=i+1;
+                    
+                    if(min_heap.get(i.first)>min_heap.get(j.first) || min_heap.get(i.second)<min_heap.get(j.second)){
+                            min_heap.pop();
+                    }
+                    ans=i.first;
+            }
+                
+            return ans;            
+    }
+};*/
