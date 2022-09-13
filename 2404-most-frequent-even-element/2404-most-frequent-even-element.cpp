18a2ab672b1c661a35a7797170e726279c36b1bf
Ayush Sharma <79182175+Ayushaff@users.noreply.github.com>
2022-09-13 09:24:25 +0530

    Time: 34 ms (82.82%), Space: 57.5 MB (79.59%) - LeetHub

--- 2404-most-frequent-even-element/2404-most-frequent-even-element.cpp
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.cpp
@@ -1,45 +1,30 @@
 class Solution {
-public:
-        //https://www.youtube.com/watch?v=fyuHcXG9iQI
-      
-    int mostFrequentEven(vector<int>& nums) {
-            map<int,int>counts;
-            
-            for(int i:nums){
-                if(i%2==0)
-                   counts[i]++;    
+    public int mostFrequentEven(int[] nums) {
+            //tc O(n)
+        HashMap<Integer,Integer>counts =new HashMap<>();
+            for(int i:nums)
+            {
+            if(i%2==0)                    
+                    counts.put(i,counts.getOrDefault(i,0)+1);
+               // if(counts.containsKey(nums[i]))
+               //      counts.put(nums[i],counts.get(nums[i])+1);
+               //  else 
+               //      counts.put(nums[i],1);                             
             }
+            int ans=-1 ,maxFreq=-1;
             
-            int ans=-1 ,freq=INT_MIN; 
+            for(Integer num:counts.keySet()){
+                    
+                if(counts.get(num)>maxFreq){
+                   maxFreq=counts.get(num);
+                        ans=num;
+                }
+                else if(counts.get(num)==maxFreq && ans>num){
+                        ans=num;                        
+                }
             
-            for(auto i: counts){
-                //cout<<i.first <<" "<<i.second<<endl;
-                    if(i.second>freq){
-                            ans=i.first, freq=i.second;
-                    }
+                    
             }
             return ans;
     }
-};
- /*     int ans;
-            unordered_map<int,int>counts;
-            priority_queue<pair<int,int>, vector<pair<int,int>>,greater<pair<int,int>>>min_heap;
-            
-            for(auto i:nums){
-                if(nums[i]%2==0)
-                   counts[i]++;    
-            }
-            for(auto &i: counts){
-                    
-                min_heap.push({i.first,i.second});
-                    int j=i+1;
-                    
-                    if(min_heap.get(i.first)>min_heap.get(j.first) || min_heap.get(i.second)<min_heap.get(j.second)){
-                            min_heap.pop();
-                    }
-                    ans=i.first;
-            }
-                
-            return ans;            
-    }
-};*/
+}
