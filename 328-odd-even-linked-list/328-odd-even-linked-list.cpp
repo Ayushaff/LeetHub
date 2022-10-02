aa305e01f436b1853860eec8c21f6c942350e458
Ayush Sharma <79182175+Ayushaff@users.noreply.github.com>
2022-10-02 21:57:53 +0530

    Time: 29 ms (22.24%), Space: 10.4 MB (95.97%) - LeetHub

--- 328-odd-even-linked-list/328-odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/328-odd-even-linked-list.cpp
@@ -10,8 +10,46 @@
  */
 class Solution {
 public:
-  ListNode* oddEvenList(ListNode* head) 
-    {
+        
+ListNode* oddEvenList(ListNode* head) 
+{
+        ListNode *oddh =NULL, *evenh=NULL, *et=NULL , *ot=NULL;
+        int c=1;
+        while(head!=NULL){
+                if(c%2==0){ //even case
+                        if(evenh==NULL){
+                                evenh=head;
+                                et=head;
+                                head=head->next;
+                        }else{
+                                et->next=head;
+                                et=et->next;
+                                head=head->next;
+                        }
+                }
+                else{ //odd case
+                        if(oddh==NULL){
+                                oddh=head;
+                                ot=head;
+                                head=head->next;
+                        }else{
+                                ot->next=head;
+                                ot=ot->next;
+                                head=head->next;
+                        }
+                }
+                c++;                
+        }
+        if(evenh==NULL) return oddh;        
+        if(oddh==NULL) return evenh;
+        
+        ot->next=evenh;
+        et->next=NULL;
+        
+        return oddh;
+}
+};
+          /*
         if(!head) return head;
         ListNode *odd=head, *evenhead=head->next, *even = evenhead;
         while(even && even->next)
@@ -24,4 +62,4 @@ public:
         odd->next = evenhead;
         return head;
     }
-};
+    */
