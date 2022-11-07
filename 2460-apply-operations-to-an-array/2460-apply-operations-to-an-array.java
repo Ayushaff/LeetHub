ee8705220c7e5e80e3a70e9c0bce0df89f485387
Ayush Sharma <79182175+Ayushaff@users.noreply.github.com>
2022-11-07 23:02:40 +0530

    Time: 2 ms (50.00%), Space: 44.4 MB (75.00%) - LeetHub

---
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.java
@@ -0,0 +1,67 @@
+class Solution {
+    public int[] applyOperations(int[] nums) {
+        //tc sc O(n) O(1)
+        int n = nums.length;
+        
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                nums[i] *= 2;
+                nums[i + 1] = 0;
+            }
+        }
+        
+        int i=0,j=0;
+        while(j<n){
+            if(nums[j]!=0)
+                nums[i++]=nums[j];
+            j++;
+        }
+        while(i<n)
+            nums[i++]=0;
+        return nums;
+    }
+}
+//2
+//tc sc O(n) O(n)
+/*
+
+  int n = nums.length;
+        
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                nums[i] *= 2;
+                nums[i + 1] = 0;
+            }
+        }
+        
+    int []ans=new int[n]; int j=0;
+     for (int i = 0; i < n - 1; i++) {
+            if(nums[i]!=0)
+                nums[j++]=nums[i];
+        }
+        return ans;
+*/
+
+//1
+
+/*
+             for (int i = 0; i < nums.length - 1; i++){
+            if (nums[i] == nums[i + 1]){
+             nums[i] *= 2;
+             nums[i + 1] = 0;
+         }
+        }
+        
+        move(nums);
+        return nums;
+    }
+    public int[] move(int arr[]){
+        int j=0;
+        for(int i=0;arr.length;i++){
+            if(arr[i]!=0){
+                arr[j++]=arr[i];
+            }
+        }
+        return arr;
+    }
+}*/
