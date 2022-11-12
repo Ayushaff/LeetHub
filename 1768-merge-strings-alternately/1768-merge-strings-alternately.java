9863b137a83aa5b82b998761a2d8c90ae5192481
Ayush Sharma <79182175+Ayushaff@users.noreply.github.com>
2022-11-12 22:30:43 +0530

    Time: 1 ms (93.41%), Space: 42 MB (79.30%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -1,6 +1,35 @@
 class Solution {
     public String mergeAlternately(String w1, String w2) {
-        int l1=w1.length() ,l2=w2.length();
+        //1pointers 
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < w1.length() || i < w2.length(); i++) {
+            if (i < w1.length())
+                res.append(w1.charAt(i));
+            if (i < w2.length())
+                res.append(w2.charAt(i));
+        }
+        return res.toString();
+    }
+}
+    /* 
+    //2pointers
+public String mergeAlternately(String w1, String w2) {
+    int n = w1.length(), m = w2.length();
+    int i = 0, j = 0;
+    StringBuilder res = new StringBuilder();
+        while (i < n || j < m) {
+            if (i < w1.length())
+                res.append(w1.charAt(i++));
+            if (j < w2.length())
+                res.append(w2.charAt(j++));
+        }
+        return res.toString();
+    }
+    */
+        
+    /*  
+    2pointers
+    int l1=w1.length() ,l2=w2.length();
         int a=l1+l2;
         char[] res=new char[a];
         int p1=0,p2=0,resp=0;
@@ -13,4 +42,4 @@ class Solution {
         }
         return new String(res);
     }
-}
+}*/
