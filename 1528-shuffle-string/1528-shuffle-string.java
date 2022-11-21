7d09bf202888eaf6c32ac630ea4e2fc2a54f3d2d
Ayush Sharma <79182175+Ayushaff@users.noreply.github.com>
2022-11-21 22:51:57 +0530

    Time: 2 ms (77.70%), Space: 44.8 MB (55.28%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.java
+++ 1528-shuffle-string/1528-shuffle-string.java
@@ -5,6 +5,8 @@ class Solution {
         for(int i=0;i<s.length();i++){
             arr[indices[i]]=s.charAt(i);
         }
-        return new String(arr);
+        String res= String.valueOf(arr);
+        return res;
+        //return new String(arr);
     }
 }
