d907a1d2b4daaf43100e2b49ce3e00b487b85bf1
Ayush Sharma <sharmayush11403@gmail.com>
2022-11-25 23:56:34 +0530

    Time: 177 ms (23.46%), Space: 85.7 MB (19.52%) - LeetHub

---
+++ 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.java
@@ -0,0 +1,37 @@
+class Solution {
+    
+    public int getSum(int n){
+        int sum=0;
+        while(n>0){
+            sum+=n%10;
+            n/=10;
+        }
+        return sum;
+    }
+    
+    public int maximumSum(int[] nums) {
+        int n=nums.length;
+            Map<Integer,List<Integer>>map=new HashMap<>();
+        //key is sum eg. 18's sum is 9 so map has key is 9 (sum of digits) and vallue is {18, 36} (integers thats is all numbers which has this  particular sum of digits) 
+        int ans=-1;
+        
+            for(int i=0;i<n;i++)
+            {
+                int currSum=getSum(nums[i]);
+                if(!map.containsKey(currSum)){  //that sum of digits is not there in map
+                    map.put(currSum,new ArrayList<>());
+                }
+                List<Integer>list=map.get(currSum);
+                list.add(nums[i]);
+                map.put(currSum,list);
+            }
+            for(Integer currSum:map.keySet()){
+                List<Integer>list=map.get(currSum);
+                if(list.size()<2)
+                    continue;
+                Collections.sort(list);
+                ans=Math.max(ans,list.get(list.size()-1)+list.get(list.size()-2));  //just want 2 pairs of integers thats is all numbers which has this  particular sum of digits) 
+            }   
+        return ans;
+    }
+}
