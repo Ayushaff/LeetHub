840ee40d0924b0c5ed10ba1eaf8da516c2e2f62f
Ayush Sharma <sharmayush11403@gmail.com>
2022-11-26 15:38:54 +0530

    Time: 91 ms (64.30%), Space: 82 MB (44.08%) - LeetHub

--- 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.java
+++ 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.java
@@ -1,6 +1,6 @@
 class Solution {
     
-    public int getSum(int n){
+    private int getSum(int n){
         int sum=0;
         while(n>0){
             sum+=n%10;
@@ -11,7 +11,7 @@ class Solution {
     
     public int maximumSum(int[] nums) {
         int n=nums.length;
-            Map<Integer,List<Integer>>map=new HashMap<>();
+            Map<Integer,Integer>map=new HashMap<>();
         //key is sum eg. 18's sum is 9 so map has key is 9 (sum of digits) and vallue is {18, 36} (integers thats is all numbers which has this  particular sum of digits) 
         int ans=-1;
         
@@ -19,19 +19,23 @@ class Solution {
             {
                 int currSum=getSum(nums[i]);
                 if(!map.containsKey(currSum)){  //that sum of digits is not there in map
-                    map.put(currSum,new ArrayList<>());
+                    map.put(currSum,nums[i]);
                 }
-                List<Integer>list=map.get(currSum);
-                list.add(nums[i]);
-                map.put(currSum,list);
+                else{
+                    ans=Math.max(ans,map.get(currSum)+nums[i]);
+                    map.put(currSum,Math.max(map.get(currSum),nums[i]));
+                }
+                // List<Integer>list=map.get(currSum);
+                // list.add(nums[i]);
+                // map.put(currSum,list);
             }
-            for(Integer currSum:map.keySet()){
-                List<Integer>list=map.get(currSum);
-                if(list.size()<2)
-                    continue;
-                Collections.sort(list);
-                ans=Math.max(ans,list.get(list.size()-1)+list.get(list.size()-2));  //just want 2 pairs of integers thats is all numbers which has this  particular sum of digits) 
-            }   
+            // for(Integer currSum:map.keySet()){
+            //     List<Integer>list=map.get(currSum);
+            //     if(list.size()<2)
+            //         continue;
+            //     Collections.sort(list);
+            //     ans=Math.max(ans,list.get(list.size()-1)+list.get(list.size()-2));  //just want 2 pairs of integers thats is all numbers which has this  particular sum of digits) 
+            // }   
         return ans;
     }
 }
