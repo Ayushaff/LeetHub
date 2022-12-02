509f0a7401de69ed884e21818e27784396650133
Ayush Sharma <sharmayush11403@gmail.com>
2022-12-02 21:49:55 +0530

    Time: 4 ms (29.17%), Space: 42.5 MB (5.89%) - LeetHub

--- 1748-sum-of-unique-elements/1748-sum-of-unique-elements.java
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.java
@@ -1,33 +1,21 @@
 class Solution {
     public int sumOfUnique(int[] nums) {
-//         Map<Integer,Integer>mp=new HashMap<>();
+        Map<Integer,Integer>mp=new HashMap<>();
         
-//         for(int i:nums)
-//             mp.put(i,mp.getOrDefault(i,0)+1);
-        
-//         // for (Map.Entry<Integer,Integer> entry : mp.entrySet()) 
-//         //     System.out.println("Key = " + entry.getKey() +
-//         //                      ", Value = " + entry.getValue());
-        
-//         int s=0;
-//         for (Map.Entry<Integer,Integer> entry : mp.entrySet()) //using map.entrySet() for iteration  
-//         {  
-//             if(entry.getValue()==1)
-//                 s+=entry.getKey();
-//         }   
-       
-//         return s;
-        
-        int arr[]=new int[101];
         for(int i:nums)
-            arr[i]++;
+            mp.put(i,mp.getOrDefault(i,0)+1);
+        
+        // for (Map.Entry<Integer,Integer> entry : mp.entrySet()) 
+        //     System.out.println("Key = " + entry.getKey() +
+        //                      ", Value = " + entry.getValue());
         
         int s=0;
-        for(int i=1;i<=100;i++)
-            if(arr[i]==1){
-                System.out.println(arr[i]);
-                s+=i;
-            }
-             return s;   
+        for (Map.Entry<Integer,Integer> entry : mp.entrySet()) //using map.entrySet() for iteration  
+        {  
+            if(entry.getValue()==1)
+                s+=entry.getKey();
+        }   
+       
+        return s;
     }
 }
