fd25c3eb32e330d2a7b185b2485bf4307102ad9e
Ayush Sharma <sharmayush11403@gmail.com>
2023-01-29 18:20:37 +0530

    Time: 1 ms (99.86%), Space: 42.2 MB (98.33%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,22 +1,24 @@
 class Solution {
-     public void func(int index,int[]nums,List<Integer>ds,List<List<Integer>>list){
-        if(index>=nums.length){
-            //List<Integer>k = new ArrayList<>(ds);
-            //Collections.sort(k);
-            //list.add(k);
-            list.add(new ArrayList<>(ds));
-            return;
+private static void f(int ind,int arr[],List<List<Integer>>ans,List<Integer>ds){
+                if(ind>=arr.length){
+                        // List<Integer>d=new ArrayList<>(ds);
+                        // Collections.sort(d);
+                        ans.add(new ArrayList<>(ds));
+                        return;
+                }
+                ds.add(arr[ind]);
+                f(ind+1,arr,ans,ds);
+                ds.remove(ds.size()-1);
+                //conditions for unique
+                while(ind<arr.length-1 && arr[ind]==arr[ind+1])
+                        ind++;
+        
+                f(ind+1,arr,ans,ds);
         }
-        ds.add(nums[index]);
-        func(index+1,nums,ds,list);
-        ds.remove(ds.size()-1);
-        while(index<nums.length-1 && nums[index]==nums[index+1])index++;
-        func(index+1,nums,ds,list);
+    public List<List<Integer>> subsetsWithDup(int[] arr) {
+        List<List<Integer>>ans=new ArrayList<>();
+            Arrays.sort(arr);
+            f(0,arr,ans,new ArrayList<>());
+            return ans;
     }
-    public List<List<Integer>> subsetsWithDup(int[] nums) {
-        List<List<Integer>> list = new ArrayList<>();
-        Arrays.sort(nums);
-        func(0,nums,new ArrayList<>(),list);
-        return list;
-    }
-} 
+}
