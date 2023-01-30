aaa7d70f232c13f35f895e5002b6153655747628
Ayush Sharma <sharmayush11403@gmail.com>
2023-01-30 16:07:16 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (68.78%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,17 +1,17 @@
 class Solution {
-        private static void f(int ind,int []arr,int n,List<List<Integer>>ans,ArrayList<Integer>ds){
-                if(ind>=n){
+        private static void f(int ind,int []arr,List<List<Integer>>ans,ArrayList<Integer>ds){
+                if(ind>=arr.length){
                         ans.add(new ArrayList<>(ds));
                         return;
                 }
                 ds.add(arr[ind]);
-                f(ind+1,arr,n,ans,ds);
+                f(ind+1,arr,ans,ds);
                 ds.remove(ds.size()-1);
-                f(ind+1,arr,n,ans,ds);        
+                f(ind+1,arr,ans,ds);        
         }
     public List<List<Integer>> subsets(int[] arr) {
         List<List<Integer>>ans=new ArrayList<>();
-            f(0,arr,arr.length,ans,new ArrayList<>());
+            f(0,arr,ans,new ArrayList<>());
             return ans;
     }
 }
