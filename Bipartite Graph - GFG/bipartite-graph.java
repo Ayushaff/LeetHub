a1935657a6d6ac140b88aaf8deb87476c5056793
Ayush Sharma <sharmayush11403@gmail.com>
2023-08-13 18:22:49 +0530

    Added solution - LeetHub

--- Bipartite Graph - GFG/bipartite-graph.java
+++ Bipartite Graph - GFG/bipartite-graph.java
@@ -34,41 +34,57 @@ class GFG
 }
 // } Driver Code Ends
 
+// Initialize an array colors[] of size V to keep track of the color of each node. Initialize all elements of the array to -1, indicating that no nodes are colored yet.
 
-class Solution
-{
-    private boolean dfs(int node,int color,int colors[],ArrayList<ArrayList<Integer>>adj){
-        colors[node]=color;
-        //traverse adj nodes of it
-        
-        for(int it:adj.get(node))
-        {
-            if(colors[it]==-1){//if not visited to that adj node
-                //just color it opposite
-                if(dfs(it,1-color,colors,adj)==false)
-                    return false;
-            }
-            //if already colored
-            else if(colors[it]==color)//check if its same colored as the node 
-                {
-                    return false;
+// 1)For each node i from 0 to V-1, if the node is not yet colored, perform the following steps:
+// a. Start a DFS traversal from node i.
+// b. During the traversal, color the current node with the color 0.
+// c. For each adjacent node it of the current node:
+
+// 2)If the adjacent node it is not visited (color is -1), recursively color it with the opposite color (1 - color).
+// If the adjacent node it is already colored and has the same color as the current node, return false indicating that the graph is not bipartite.
+// d. If the traversal completes without any conflicts, return true indicating that the current component is bipartite.
+
+// 3)If all components in the graph are found to be bipartite, return true. Otherwise, return false.
+
+// In simple terms, the algorithm colors the nodes in the graph with two different colors, ensuring that no two adjacent nodes have the same color. If it is possible to color all nodes in such a way, then the graph is bipartite.
+
+// The DFS traversal explores each connected component of the graph and colors the nodes while checking for conflicts. If a conflict is found during the traversal, the graph is not bipartite. If the traversal completes without conflicts for all components, then the graph is bipartite.
+class Solution {
+    // Helper function for DFS traversal
+    private boolean dfs(int node, int color, int colors[], ArrayList<ArrayList<Integer>> adj) {
+        colors[node] = color; // Color the current node with the specified color
+
+        // Traverse adjacent nodes
+        for (int it : adj.get(node)) {
+            if (colors[it] == -1) { // If the adjacent node is not visited
+                // Recursively color the adjacent node with the opposite color
+                if (!dfs(it, 1 - color, colors, adj)) {
+                    return false; // If coloring is not possible, return false
                 }
-        }
-        return true;
-    }
-    public boolean isBipartite(int V, ArrayList<ArrayList<Integer>>adj)
-    {
-        // Code here
-        int colors[]=new int [V];
-        for(int i=0;i<V;i++)colors[i]=-1;
-        // Arrays.fill(colors,-1);
-        
-        for(int i=0;i<V;i++){
-            if(colors[i]==-1){
-                if(dfs(i,0,colors,adj)==false)
-                    return false;
+            } else if (colors[it] == color) {
+                return false; // If the adjacent node has the same color as the current node, return false
             }
         }
-        return true;
+        return true; // If coloring is successful, return true
+    }
+
+    // Main function to check if the graph is bipartite
+    public boolean isBipartite(int V, ArrayList<ArrayList<Integer>> adj) {
+        int colors[] = new int[V]; // Array to store colors of nodes
+        for (int i = 0; i < V; i++) {
+            colors[i] = -1; // Initialize all nodes as uncolored
+        }
+
+        // Check each component in the graph
+        for (int i = 0; i < V; i++) {
+            if (colors[i] == -1) { // If the node is uncolored
+                // Start DFS traversal from this node and color it with color 0
+                if (!dfs(i, 0, colors, adj)) {
+                    return false; // If coloring is not possible, graph is not bipartite
+                }
+            }
+        }
+        return true; // If all components are bipartite, return true
     }
 }
