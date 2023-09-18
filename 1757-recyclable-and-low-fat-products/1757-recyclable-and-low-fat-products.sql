11dcb639f0cf67328102f6b6bc1dafbe41400a36
Ayush Sharma <sharmayush11403@gmail.com>
2023-09-18 22:11:34 +0530

    Time: 852 ms (99.76%), Space: 0B (100.00%) - LeetHub

---
+++ 1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.sql
@@ -0,0 +1,3 @@
+# Write your MySQL query statement below
+#have to write product id so select [column name] name from table WHERE lowfats and recyclabe are true
+SELECT product_id from Products WHERE low_fats='Y' && recyclable='Y'
