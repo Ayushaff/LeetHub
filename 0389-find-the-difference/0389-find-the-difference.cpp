8115300fb3cae4e8bf86a999dc08e5a136ca5ff4
Ayush Sharma <sharmayush11403@gmail.com>
2023-09-27 21:27:43 +0530

    Time: 4 ms (45.92%), Space: 7.1 MB (16.88%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
+
     char findTheDifference(string s, string t) {
-        unordered_map<char,int>m;
-        char ans;
-        for(int i=0;i<s.length()+1;i++)
-        {
-            m[t[i]]++;
-            m[s[i]]--;
-        }
-        for(auto it:m){
-        if(it.second==1)
-            ans=it.first;
-            }
-        return ans;
+        int x=0;
+        for(auto c:s)x^=c;
+        for(auto c:t)x^=c;
+        return x;
+        // unordered_map<char,int>m;
+        // char ans;
+        // for(int i=0;i<s.length()+1;i++)
+        // {
+        //     m[t[i]]++;
+        //     m[s[i]]--;
+        // }
+        // for(auto it:m){
+        // if(it.second==1)
+        //     ans=it.first;
+        //     }
+        // return ans;
   
     }
 };
