dc074b4ea10d744b065a6c9881c83a2f6b4db311
Ayush Sharma <sharmayush11403@gmail.com>
2023-10-07 21:26:44 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (41.00%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -3,12 +3,12 @@ private:
     void solve(string digit, string output, int index, vector<string>& ans, string mapping[] ) {
         
         //base case
-        if(index >= digit.length()) {
+        if(index == digit.length()) {
             ans.push_back(output);
             return;
         }
         
-        int number = digit[index] - '0';
+        int number = digit[index] - '0';//minus '0' will convert char into int
         string value = mapping[number];
         
         for(int i=0; i<value.length(); i++) {
@@ -23,6 +23,7 @@ public:
         vector<string> ans;
         if(digits.length()==0)
             return ans;
+        
         string output;
         int index = 0;
         string mapping[10] = {"", "", "abc", "def", "ghi", "jkl","mno","pqrs","tuv","wxyz"};
