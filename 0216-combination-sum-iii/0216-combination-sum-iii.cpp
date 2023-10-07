d7594fa0fa98072795724e0c4a82f6884aed4434
Ayush Sharma <sharmayush11403@gmail.com>
2023-10-07 17:29:26 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (46.24%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,22 +1,40 @@
 class Solution {
 public:
-    void p(int i,int k,int tar,vector<vector<int>>&ans,vector<int>&ds){
-        //bc
+    void p(int ele,int k,int tar,vector<vector<int>>&ans,vector<int>&ds){
+        
         if(k==0){
             if(tar==0){
                 ans.push_back(ds);
             }
             return;
         }
-        
-        if(i<=9){
+    for(int i=ele;i<=9;i++){
+        if(i<=tar){
             ds.push_back(i);
             p(i+1,k-1,tar-i,ans,ds);
-            
             ds.pop_back();
-            p(i+1,k,tar,ans,ds);
         }
+        
     }
+}
+    
+        //bc
+//         if(k==0){
+//             if(tar==0){
+//                 ans.push_back(ds);
+//             }
+//             return;
+//         }
+        
+//         if(ele<=9){
+            
+//             ds.push_back(i);
+//             p(ele+1,k-1,tar-ele,ans,ds);
+            
+//             ds.pop_back();
+//             p(ele+1,k,tar,ans,ds);
+        // }
+    // }
     vector<vector<int>> combinationSum3(int k, int n) {
         //approach 1 pick and no pick
         vector<vector<int>>ans;
